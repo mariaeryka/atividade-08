ee58519fc66518d01b40b5f5788eeeaa141af2fc
mariaeryka <erykamariab4@gmail.com>
2024-09-09 09:46:28 -0300

    dolar

---
+++ atividade08.py
@@ -0,0 +1,4 @@
+# Imagine que você está em uma viagem para os Estados Unidos e precisa converter o valor em reais para dólares. Crie um programa que receba o valor em reais e a taxa de câmbio atual, e exiba o valor equivalente em dólares.
+reais=int(input("quantos reais você tem?"))
+dolares= 0.18
+print(F"seu dinheiro em Dolar é dolar")
